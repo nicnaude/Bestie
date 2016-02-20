c8239ef558236659b39258a8d2724e117c4863d3
michaelsaltzman <michaelasaltzman@gmail.com>
2016-02-20 14:33:13 -0800

    Mike's first commit from Source Tree.

---
+++ Bestie/BestieUser.swift
@@ -0,0 +1,9 @@
+//
+//  BestieUser.swift
+//  Bestie
+//
+//  Created by Michael Saltzman on 2/19/16.
+//  Copyright © 2016 Nicholas Naudé. All rights reserved.
+//
+
+import Foundation
---
+++ Bestie/UserCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserCell.swift
+//  Bestie
+//
+//  Created by Michael Saltzman on 2/19/16.
+//  Copyright © 2016 Nicholas Naudé. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
